c96fbc021b229aa6f752a37e59d1a4627e4c95fe
Mike <athers_05@hotmail.co.uk>
2017-09-09 14:39:53 +0100

    Add project files.

---
+++ DLS_Technologies/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DLS_Technologies.Startup))]
+namespace DLS_Technologies
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
