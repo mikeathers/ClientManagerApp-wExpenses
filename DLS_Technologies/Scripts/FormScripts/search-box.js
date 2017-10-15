6eb659a35162ab816eacc1bd6356a0d2a4a61984
Mike <athers_05@hotmail.co.uk>
2017-10-15 23:39:15 +0100

    Added second searchbox

--- DLS_Technologies/Scripts/FormScripts/search-box.js
+++ DLS_Technologies/Scripts/FormScripts/search-box.js
@@ -26,6 +26,31 @@ $(document).ready(function () {
             window.location.href = "/Customers/GetCustomer/" + ui.item.value;
         }
     })
+
+    $('#custSearchBox').autocomplete({
+        source: function (request, response) {
+            $.ajax({
+                url: "/Customers/AutoComplete",
+                data: {
+                    custName: request.term
+                },
+                dataType: "json",
+                success: function (data) {
+                    console.log(data);
+                    response($.map(data, function (item) {
+                        return {
+                            label: item.name,
+                            value: item.id
+                        };
+                    }));
+                }
+            });
+        },
+        minLength: 1, // require at least one character from the user
+        select: function (event, ui) {
+            window.location.href = "/Customers/GetCustomer/" + ui.item.value;
+        }
+    })
     
 })
 
